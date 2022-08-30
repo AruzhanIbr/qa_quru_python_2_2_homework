4fb42dcc6825f3dddb3e3d7814fdb86e425aa561
AruzhanIbr <ibragimova080499@gmail.com>
2022-08-30 16:30:21 +0600

    Прописаа параметры фикстур

---
+++ conftest.py
@@ -0,0 +1,10 @@
+from selene.support.shared import browser
+from selene import be, have
+import pytest
+@pytest.fixture(scope='session', autouse=True)
+def open_browser():
+    browser.config.window_height = 458
+    browser.config.window_width = 998
+    browser.open('https://google.com')
+    yield
+    browser.close()
