826659f30f0f3fd9c2bb934838402de2b81dc1fa
AruzhanIbr <ibragimova080499@gmail.com>
2022-08-30 20:54:18 +0600

    Убрала лишние импорты, разделила блоки пробелами, сделала

--- hw_2_lesson_positive_test.py
+++ hw_2_lesson_positive_test.py
@@ -1,11 +1,9 @@
-
 from selene.support.shared import browser
 from selene import be, have
-import pytest
 
-#фикстуры в файле conftest
+
+# фикстуры в файле conftest
 def test_first():
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
-    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
-
-
+    browser.element('[id="search"]').should(
+        have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
