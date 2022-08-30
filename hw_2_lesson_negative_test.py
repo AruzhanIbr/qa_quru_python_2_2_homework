8eadbbefd7e50c5f204af3365c75227e5eac3228
AruzhanIbr <ibragimova080499@gmail.com>
2022-08-30 20:53:20 +0600

    Убрала лишние импорты, разделила блоки пробелами, сделала так чтобы тест не падал

--- hw_2_lesson_negative_test.py
+++ hw_2_lesson_negative_test.py
@@ -1,11 +1,9 @@
-
 from selene.support.shared import browser
-from selene import be, have
-import pytest
+from selene import have, be
 
-#фикстуры в файле conftest
+
+# фикстуры в файле conftest
 def test_negative():
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
-    browser.element('[id="search"]').should(have.text('yashaka/selenide: User-oriented Web UI browser tests in Python'))
-
-
+    browser.element('[id="search"]').should(
+        have.no.text('yashaka/selenide: User-oriented Web UI browser tests in Python'))
